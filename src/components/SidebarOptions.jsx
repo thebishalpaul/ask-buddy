c81c371fd73219ef7e37c3cc679ff9aeb1aaa64b
Bishal Paul <bishalpaul34@gmail.com>
2023-09-18 02:20:59 +0530

    sidebar additions 2

--- src/components/SidebarOptions.jsx
+++ src/components/SidebarOptions.jsx
@@ -3,14 +3,13 @@ import "./css/SidebarOptions.css"
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { useState } from "react"
 
-
 function SidebarOptions({ item }) {
     const [open, setOpen] = useState(false)
-
 
     if (item.childrens) {
         return (
             <div className={open ? "sidebar-item open" : "sidebar-item"}>
+                {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/RedCat_8727.jpg/1280px-RedCat_8727.jpg" alt='' height={20} width={20}/> */}
                 <div className="sidebar-title">
                     {/* Parent Menu */}
                     <span>
@@ -28,11 +27,12 @@ function SidebarOptions({ item }) {
         )
     } else {
         return (
-            <a href={item.path || "#"} className="sidebar-item plain">
-                {item.icon && <img src={item.icon} alt=''/>
-                }
-                {item.title}
-            </a>
+            <div className="sidebarItem">
+                {item.icon && <img src={item.icon} alt='' height={50} width={60} />}
+                <a href={item.path || "#"} className="sidebar-item plain">
+                    {item.title}
+                </a>
+            </div>
         )
     }
 }
