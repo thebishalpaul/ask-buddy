daf149d7955b5c7f2611c52b0d26db433f4e3e3e
Bishal Paul <bishalpaul34@gmail.com>
2023-11-26 16:32:03 +0530

    popup issue resolved

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth,GoogleAuthProvider} from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,5 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-
-export{auth,provider};
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+export { auth, provider };
